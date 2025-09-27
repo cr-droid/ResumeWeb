e2a6629008f847435f20015b25c32e68135352ad
Christian Ruiz <chris@Christians-MacBook-Air.local>
2025-09-27 01:54:29 -0700

    Updates

--- _next/static/UN2_WqKUUT-UIbNmi9tk2/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/203-1ebf859ee3b71479.js","static/chunks/pages/index-a91a70106a81394c.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
